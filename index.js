0eadf48c1e79fe36bd5db13062448808bfd8a3cf
Karan <kairakaran362@gmail.com>
2021-07-30 18:58:07 +0530

    added comments and refactor

--- index.js
+++ index.js
@@ -1,96 +1,113 @@
-const dropZone=document.querySelector('.dropbox');
-const fileOpener= document.querySelector('#fileopener');
-const file_selector_btn=document.querySelector('.file_selector_btn');
-const progessContainer=document.querySelector('.progress_container')
+const dropZone = document.querySelector('.dropbox');
+const fileOpener = document.querySelector('#fileopener');
+const file_selector_btn = document.querySelector('.file_selector_btn');
+const progessContainer = document.querySelector('.progress_container');
 
-const uploadedPercentage=document.querySelector('#value');
-const progressBar=document.querySelector('.progress_bar')
-const showURL=document.querySelector('#fileURL');
-const sharingContainer=document.querySelector('.sharing_container');
-const copyButton=document.querySelector('#copy');
-const copyMessage=document.querySelector('.msg');
+const uploadedPercentage = document.querySelector('#value');
+const progressBar = document.querySelector('.progress_bar');
+const showURL = document.querySelector('#fileURL');
+const sharingContainer = document.querySelector('.sharing_container');
+const copyButton = document.querySelector('#copy');
+const copyMessage = document.querySelector('.msg');
 
-const host="https://karan-kaira-sharex.herokuapp.com/";
+const host = 'https://karan-kaira-sharex.herokuapp.com/';
 // const host="http://localhost:5000/"
-const uploadURL=host+"api/file";
+const uploadURL = host + 'api/file';
 
-dropZone.addEventListener('dragover',(e)=>{
-    e.preventDefault();
+dropZone.addEventListener('dragover', (e) => {
+  //? event listener for drag and drop over element
 
-    if(!dropZone.classList.contains('dragged'))
-        dropZone.classList.add('dragged')
-})
+  e.preventDefault(); //? default behaviour is dowloading the file,we don't want that
 
-dropZone.addEventListener('dragleave',(e)=>{
-    if(dropZone.classList.contains('dragged'))
-        dropZone.classList.remove('dragged');
-})
+  if (!dropZone.classList.contains('dragged'))
+    dropZone.classList.add('dragged');
+});
 
-dropZone.addEventListener('drop',(e)=>{
-    e.preventDefault();
+dropZone.addEventListener('dragleave', (e) => {
+  //? event listener when u move away
+  if (dropZone.classList.contains('dragged'))
+    dropZone.classList.remove('dragged');
+});
 
-    if(dropZone.classList.contains('dragged'))
-       dropZone.classList.remove('dragged');
-      const files=e.dataTransfer.files;
-       if(files.length)
-         {
-             fileOpener.files=files; 
-             upload();
-         }
-})
+dropZone.addEventListener('drop', (e) => {
+  //? when file dropped on the box
 
-file_selector_btn.addEventListener('click',(e)=>{
-  fileOpener.click();
-})
+  e.preventDefault();
 
-fileOpener.addEventListener("change",()=>{
+  if (dropZone.classList.contains('dragged'))
+    dropZone.classList.remove('dragged');
+  const files = e.dataTransfer.files;
+  if (files.length) {
+    //? when files dropped on the box , then attach those files to the choose file button
+    fileOpener.files = files;
     upload();
-})
+  }
+});
 
-const upload=()=>{
-    const file=fileOpener.files[0];
-    const formData=new FormData();
+file_selector_btn.addEventListener('click', (e) => {
+  //? transfer click to the button
+  fileOpener.click();
+});
 
-     formData.append("myFile",file);
-    
-     const xhr=new XMLHttpRequest();
-     xhr.onreadystatechange=()=>{
-           if(xhr.readyState===XMLHttpRequest.DONE){
-            //console.log(xhr.response);  
-            showLink(JSON.parse(xhr.response));
-           }       
-     }
-     document.querySelector('.progress_container').style.display="block";
-     xhr.upload.onprogress=progressReport;
-     xhr.open('POST',uploadURL );
-     xhr.send(formData);
+fileOpener.addEventListener('change', () => {
+  //? when file uploaded through browse and not through drag and drop
+  upload();
+});
+
+const upload = () => {
+  const file = fileOpener.files[0]; //? only 1 file allowed
+  const formData = new FormData();
+
+  formData.append('myFile', file);
+
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      //console.log(xhr.response);
+      //? show the link which we get from backend
+      showLink(JSON.parse(xhr.response));
+    }
+  };
+  //? because initially progress bar was hidden
+  progessContainer.style.display = 'block';
+   
+  xhr.upload.onprogress = progressReport;//? see upload report 
+  xhr.open('POST', uploadURL);
+  xhr.send(formData);
 };
 
-const progressReport=(e)=>{
-    progressBar.style.transform=`scaleX(0)`;
-   progessContainer.style.display="block"
-    const perc=parseInt((e.loaded/e.total)*100);
-   console.log(perc);
-   
-    uploadedPercentage.innerText=perc;
-    progressBar.style.transform=`scaleX(${perc/100})`;
-}
+const progressReport = (e) => {
+  progressBar.style.transform = `scaleX(0)`;
+  progessContainer.style.display = 'block';
+  const perc = parseInt((e.loaded / e.total) * 100);
+  console.log(perc);
 
-const showLink=(link)=>{
-    sharingContainer.style.display="block";
-   //console.log(link.file);
-   document.querySelector('.progress_container').style.display="none";
-    showURL.value=link.file;
-}
+  uploadedPercentage.innerText = perc;
+  progressBar.style.transform = `scaleX(${perc / 100})`;
+};
 
-copyButton.addEventListener('click',()=>{
+const showLink = (link) => {
+  sharingContainer.style.display = 'block';
+  //console.log(link.file);
+  //? now hide the progress bar to show the received link
+  progessContainer.style.display = 'none';
+  showURL.value = link.file;
+};
+
+//? paste to clipboard
+copyButton.addEventListener('click', () => {
+
+    //? select the link
     showURL.select();
-    document.execCommand('copy');
-    copyMessage.style.display='block';
-    window.scrollBy(0,200)
-    let copyMsg;
-    clearTimeout(copyMsg);
-    copyMsg=setTimeout(()=>{
-        copyMessage.style.display='none';
-    },3000);
-})
+
+  document.execCommand('copy'); //? copy everything which is selected
+
+  copyMessage.style.display = 'block';
+  window.scrollBy(0, 200);
+  let copyMsg;
+  clearTimeout(copyMsg);
+  //? remove the 'copy to clipboard' after 3 seconds
+  copyMsg = setTimeout(() => {
+    copyMessage.style.display = 'none';
+  }, 3000);
+});
